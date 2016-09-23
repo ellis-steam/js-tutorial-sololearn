c3a4622a6eb365d6a3b626346c9af935f7003e72
Ellis <ellis@s-team.com.tw>
2016-09-23 13:39:05 +0800

    String Operators

--- demo.js
+++ demo.js
@@ -1,2 +1,3 @@
-var sayHello = 'Hello world! \'I am a JavaScript programmer.\'';
-document.write(sayHello);
+var mystring1 = "I am learning ";
+var mystring2 = "JavaScript with SoloLearn.";
+document.write(mystring1 + mystring2);
