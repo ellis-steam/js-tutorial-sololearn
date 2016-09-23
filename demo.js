94c24aad672ae59263bd140b08bfd0b35a5e069d
Ellis <ellis@s-team.com.tw>
2016-09-23 14:00:33 +0800

    The else Statement

--- demo.js
+++ demo.js
@@ -1,5 +1,7 @@
 var myNum1 = 7;
 var myNum2 = 10;
-if (myNum1 < myNum2) {
-  alert("JavaScript is easy to learn.");
+if (myNum1 > myNum2) {
+  alert("This is my first condition");
+} else {
+  alert("This is my second condition");
 }
